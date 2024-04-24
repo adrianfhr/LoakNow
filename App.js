c4bf4221e0628afebb7ef2a10f60c51f6f972207
adrianfhr <adrianfahriaffandi@gmail.com>
2024-04-24 18:26:25 +0700

    setup tailwind and paper

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import MyComponent from './components/AppBar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+    <View className="flex-1 items-center justify-center bg-red">
+      <View className="bg-blue-500 text-white font-bold rounded-lg border shadow-lg p-10">
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+        <MyComponent/>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
