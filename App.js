aaebadd57b3064493dc46ba26f2b9f0a1a6ca57f
adrianfhr <adrianfahriaffandi@gmail.com>
2024-05-20 01:19:13 +0700

    feat FCM

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
@@ -6,11 +7,65 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import LandingScreen from './screens/LandingScreen';
 import ProfileScreen from './screens/ProfileScreen';
-// import FCMScreen from './screens/FCMScreen'
+import messaging from '@react-native-firebase/messaging';
+import { Alert } from 'react-native';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if(requestUserPermission()){
+      messaging()
+        .getToken()
+        .then(token => {
+        console.log(token);
+      });
+    } else {
+      console.log('Permission not granted', authStatus);
+    }
+
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          );
+        }
+      });
+    
+    // Asume a message-notification contains a "type" property in the data payload of the screen to open
+    messaging().onNotificationOpenedApp(remoteMessage => {
+      console.log(
+        'Notification caused app to open from background state:',
+        remoteMessage.notification,
+      );
+    });
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
       <NavigationContainer>
           <Stack.Navigator initialRouteName="Home">
--- screens/FCMScreen.js
+++ screens/FCMScreen.js
@@ -1,59 +1,31 @@
-// import React, { useState, useEffect } from 'react';
-// import { View, Text, Alert, Platform } from 'react-native';
-// import { getMessaging, getToken, Permission } from 'firebase/messaging';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Alert, Platform } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 
-// const FcmScreen = () => {
-//   const [fcmToken, setFcmToken] = useState(null);
+PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
 
-//   useEffect(() => {
-//     // Check for required permissions on iOS
-//     if (Platform.OS === 'ios') {
-//       requestNotificationPermissions();
-//     } else {
-//       // Android doesn't require explicit permission requests
-//       retrieveFcmToken();
-//     }
-//   }, []);
+const FCMScreen = () => {
+    const [token, setToken] = useState('');
 
-//   const requestNotificationPermissions = async () => {
-//     try {
-//       const authStatus = await getMessaging().requestPermission();
-//       if (authStatus === Permission.GRANTED) {
-//         console.log('Notification permission granted');
-//         retrieveFcmToken();
-//       } else {
-//         console.log('Notification permission denied');
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       Alert.alert('Error', 'Failed to request notification permissions');
-//     }
-//   };
+    useEffect(() => {
+        // Get the device token
+        messaging()
+            .getToken()
+            .then(token => {
+                setToken(token);
+            });
 
-//   const retrieveFcmToken = async () => {
-//     try {
-//       const token = await getToken(messaging, {
-//         vapidKey: 'YOUR_VAPID_KEY', // Replace with your VAPID key for web notifications
-//       });
+        // Listen to whether the token changes
+        return messaging().onTokenRefresh(token => {
+            setToken(token);
+        });
+    }, []);
 
-//       console.log('FCM Token:', token);
-//       setFcmToken(token);
-//     } catch (error) {
-//       console.error(error);
-//       Alert.alert('Error', 'Failed to retrieve FCM token');
-//     }
-//   };
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       {fcmToken ? (
-//         <Text>Your FCM Token: {fcmToken}</Text>
-//       ) : (
-//         <Text>Fetching FCM Token...</Text>
-//       )}
-//     </View>
-//   );
-// };
-
-// export default FcmScreen;
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Device FCM Token: {token}</Text>
+        </View>
+    );
+}
